da0fee3e7f9751da7c8cd948bae1a689a84fb889
Ryan W <ryan_645@outlook.com>
2020-03-02 13:38:45 -0500

    Add project files.

---
+++ INFO2300_Assignment03/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace INFO2300_Assignment03.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
